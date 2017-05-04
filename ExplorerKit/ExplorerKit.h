0e6f2ceebbb7d0e968402778c3aa3b2eb2dc9616
Steve Wight <stevendouglaswight@gmail.com>
2017-05-04 21:04:01 +0200

    Initial Commit

---
+++ ExplorerKit/ExplorerKit.h
@@ -0,0 +1,19 @@
+//
+//  ExplorerKit.h
+//  ExplorerKit
+//
+//  Created by Steve on 5/4/17.
+//  Copyright © 2017 Steve Wight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExplorerKit.
+FOUNDATION_EXPORT double ExplorerKitVersionNumber;
+
+//! Project version string for ExplorerKit.
+FOUNDATION_EXPORT const unsigned char ExplorerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExplorerKit/PublicHeader.h>
+
+
